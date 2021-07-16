0c0a9802856cdf776468e5ea0ea1419ba9811b40
Gianni333 <giannirhino@yahoo.co.uk>
2021-07-16 22:01:12 +0100

    Added contacts test using localstorage method

---
+++ cypress/integration/training_api_tests/get_contacts.js
@@ -0,0 +1,20 @@
+describe('Get contact', () => {
+    before(()=>{
+        cy.authenticate()
+    });
+    it("returns contact successfully", () => {
+        cy.getLocalStorage('auth_token').then((auth_token) => {
+            return cy.request({
+                method: 'GET',
+                url: Cypress.env('ccBaseUrl')+'/v2/contacts/find',
+                headers: {
+                    'X-Auth-Token': auth_token
+                },
+                body: {}
+            }).then((response)=> {
+                expect(response.status).to.eq(200),
+                expect(response.body).have.property('contacts')
+            });
+        });
+    });
+})
